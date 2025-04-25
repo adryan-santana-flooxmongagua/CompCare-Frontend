07c8dd571cf8f359235a2b4de93a38438f63d04d
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-25 10:39:07 -0300

    refactor: update Menu.jsx layout structure

--- App/src/components/layout/Menu.jsx
+++ App/src/components/layout/Menu.jsx
@@ -7,7 +7,9 @@ const Menu = () => {
       <Link to="/">Home</Link>
       <Link to="/vagas">Vagas</Link>
       <Link to="/leaderboard">Ranking</Link>
+      <Link to="/cadastro">Cadastro Voluntário</Link>
       <Link to="/login">Login</Link>
+      <Link to="/register">Registrar</Link>
     </nav>
   )
 }
