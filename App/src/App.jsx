6c1f08cc423716b37412df28e303a4b77c696b2e
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-25 10:38:59 -0300

    refactor: update App.jsx to integrate new authentication routes

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/routepublic/Home'
 import VagasPublicas from './pages/routepublic/VagasPublicas'
 import CadastroVoluntario from './pages/routepublic/CadastroVolunter'
 import LoginPage from './pages/routepublic/LoginPage'
+import RegisterPage from './pages/routepublic/RegisterPage'
 import Leaderboard from './pages/routepublic/LeaderBoard'
 import DashboardHospital from './pages/routeadmin/DashboardHosp'
 import CriarVaga from './pages/routeadmin/CriarVaga'
@@ -24,9 +25,10 @@ const App = () => {
         <Route path="/vagas" element={<VagasPublicas />} />
         <Route path="/cadastro" element={<CadastroVoluntario />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
 
-        {/* Rotas protegidas (hospitais/admins) */}
+        {/* Rotas protegidas */}
         <Route path="/admin/dashboard" element={<PrivateRoute><DashboardHospital /></PrivateRoute>} />
         <Route path="/admin/criar-vaga" element={<PrivateRoute><CriarVaga /></PrivateRoute>} />
         <Route path="/admin/usuarios" element={<PrivateRoute><GerenciarUsuarios /></PrivateRoute>} />
