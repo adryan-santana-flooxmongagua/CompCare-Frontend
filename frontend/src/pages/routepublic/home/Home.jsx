dab05d518259ec90d54fedbf2c0c72fadbf7a4e7
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-30 09:02:55 -0300

    feat: add basic homepage custom styles

--- frontend/src/pages/routepublic/home/Home.jsx
+++ frontend/src/pages/routepublic/home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import heroImage from '../../../assets/imgs/images.jpg';
 import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Bem-vindo ao CompCare</h1>
-      <p>
-        O <strong>CompCare</strong> é uma plataforma dedicada à conexão entre hospitais e voluntários 
-        dispostos a fazer a diferença. Aqui, você pode se cadastrar, visualizar oportunidades e oferecer seu 
-        tempo e carinho para quem mais precisa. Juntos, transformamos empatia em ação.
-      </p>
+    <div className="hero-container">
+      <div className="hero-text">
+        <h1>Sua conexão entre <br /> hospitais e voluntários</h1>
+        <p>Transformando empatia em ação com a CompCare</p>
+        <a href="/vagas" className="hero-button">
+          Ver vagas disponíveis
+        </a>
+      </div>
+      <div className="hero-image">
+        <img src={heroImage} alt="CompCare apresentação" />
+      </div>
     </div>
   );
 };
