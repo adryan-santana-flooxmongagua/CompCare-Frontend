44755622609263495abaf4868298f7107c85d30d
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-05-05 22:59:15 -0300

    chore: update API endpoint

--- Frontend/src/pages/routepublic/register/Register.jsx
+++ Frontend/src/pages/routepublic/register/Register.jsx
@@ -28,7 +28,7 @@ const Register = () => {
 
     try {
       setLoading(true);
-      const response = await fetch('https://compcare-backend.onrender.com/api/auth/register', {
+      const response = await fetch('http://localhost:5000/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name: nome, email, password: senha, role })
