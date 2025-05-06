4b16330f742c217210c6e57d03bf94b1b103a2ad
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-05-06 15:36:15 -0300

    chore: rename file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/layout/Header";
 import Homes from "./pages/routepublic/homes/Homes";
-import VagasPublicas from "./pages/routepublic/vagapub/VagasPublicas";
+import VagasPublicas from "./pages/routepublic/vagapub/Vagapub";
 import CadastroVoluntario from "./pages/routepublic/voluntario/CadastroVolunter";
 import LoginPage from "./pages/routepublic/login/LoginPage";
 import RegisterPage from "./pages/routepublic/register/RegisterPage";
